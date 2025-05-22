d2eb02a0f3ad1cf5b22d24bd5d582890a53fb8be
Ekzegit <exe.acev@hotmail.com>
2025-05-22 14:55:26 -0400

    Deploy limpio en gh-pages

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-    base: '/KameHouseDev/', // 👈 nombre del repositorio en GitHub
-    plugins: [react()],
-})
