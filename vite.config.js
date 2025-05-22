f3912d3a50bf7bbdd71ba18aeabdd141a7235b83
Ekzegit <exe.acev@hotmail.com>
2025-05-22 12:31:46 -0400

    cambios para pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    base: '/KameHouseDev/', // 👈 nombre del repositorio en GitHub
+    plugins: [react()],
 })
