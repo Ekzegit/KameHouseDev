d83e12fcdd9c91897cd837f76b0de1bc78b611c2
Ekzegit <exe.acev@hotmail.com>
2025-05-22 23:43:47 -0400

    commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    base: '/kamehousedev/', // 👈 nombre del repositorio en GitHub
+    base: '/',
     plugins: [react()],
+    publicDir: 'public' // 👈 forzamos la carpeta public manualmente
 })
