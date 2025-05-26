8f584502cbd2ba4f0f12987f8d538717f6112680
Ekzegit <exe.acev@hotmail.com>
2025-05-26 10:28:59 -0400

    favi

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-scroll";
 import { motion } from "framer-motion";
-import nubeKame from "https://kamehousedev.cl/nubekame.webp"; // Asegúrate de que esta ruta sea correcta
+import nubeKame from "../asset/nubekame.webp"; // Asegúrate de que esta ruta sea correcta
 
 function Header() {
     const [isOpen, setIsOpen] = useState(false);
